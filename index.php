fd9b5868d70324a279ddc292e67f29c7e7a68745
kbax6342 <k3vin.baxt3r@gmail.com>
2023-03-21 12:42:48 -0400

    change

--- index.php
+++ index.php
@@ -1,19 +1,30 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "root";
+// $host = "localhost";
+// $username = "root";
+// $password = "root";
 
-try
-{
-    $conn = new PDO("mysql:host=$host;dbname=survey", $username, $password);
+// try
+// {
+//     $conn = new PDO("mysql:host=$host;dbname=survey", $username, $password);
 
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "Connected successfully";
-}
-catch(PDOException $e)
-{
-    echo "Connection failed: " . $e->getMessage();
-}
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     //echo "Connected successfully";
+// }
+// catch(PDOException $e)
+// {
+//     echo "Connection failed: " . $e->getMessage();
+// }
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 $fnameErr = $lnameErr = $emailErr = "";
 $fname = $lname = $email = "";
